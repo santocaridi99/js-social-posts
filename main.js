3aee8e82a18476ef4ae22e02318bc573433ce52a
santocaridi99 <santocaridi99@gmail.com>
2022-01-11 15:23:34 +0100

    creazione prima milestone di prova

---
+++ main.js
@@ -0,0 +1,30 @@
+//creo array di post
+const library = [
+    {
+        id:1,
+        name:'Anonimo',
+        photo:'https://unsplash.it/300/300?image=15',
+        data:'(11/01/2022)',
+        testo:'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+        image:'https://unsplash.it/600/300?image=171',
+        likes:80,
+    },
+    {
+        id:2,
+        name:'Anonimo2',
+        photo:'https://unsplash.it/300/300?image=15',
+        data:'(11/01/2022)',
+        testo:'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+        image:'https://unsplash.it/600/300?image=171',
+        likes:40,
+    },
+    {
+        id:3,
+        name:'Anonimo3',
+        photo:'https://unsplash.it/300/300?image=15',
+        data:'(11/01/2022)',
+        testo:'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+        image:'https://unsplash.it/600/300?image=171',
+        likes:65,
+    },
+] 
