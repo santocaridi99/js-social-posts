55edbc24cc9be34efa4a980b835a323c30686baf
santocaridi99 <santocaridi99@gmail.com>
2022-01-11 15:46:34 +0100

    seconda milestone

--- main.js
+++ main.js
@@ -28,3 +28,46 @@ const library = [
         likes:65,
     },
 ] 
+//collego il contenitore dei post
+const postContainer = document.querySelector('.posts-list');
+//creo una function che mi permette di creare i post
+function createPost (container , librayList){
+    //ciclo for che scorre tutto l'array e dove ricavo i dati da mandare all'html
+    for(let i=0 ;i<librayList.length ; i++){
+        const post=librayList[i];
+        container.innerHTML+=`
+                <div class="post">
+                <div class="post__header">
+                    <div class="post-meta">                    
+                        <div class="post-meta__icon">
+                            <img class="profile-pic" src="${post.photo}" alt="${post.name}">                    
+                        </div>
+                        <div class="post-meta__data">
+                            <div class="post-meta__author">${post.name}</div>
+                            <div class="post-meta__time">${post.data}</div>
+                        </div>                    
+                    </div>
+                </div>
+                <div class="post__text">${post.testo}</div>
+                <div class="post__image">
+                    <img src="${post.image}" alt="">
+                </div>
+                <div class="post__footer">
+                    <div class="likes js-likes">
+                        <div class="likes__cta">
+                            <a class="like-button  js-like-button" href="#" data-postid="${post.id}">
+                                <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                                <span class="like-button__label">Mi Piace</span>
+                            </a>
+                        </div>
+                        <div class="likes__counter">
+                            Piace a <b id="like-counter-1" class="js-likes-counter">${post.likes}</b> persone
+                        </div>
+                    </div> 
+                </div>            
+            </div> `
+
+    }
+}
+
+createPost(postContainer,library);
